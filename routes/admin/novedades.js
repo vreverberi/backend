ce066007e5677e6b281691e6fed8e5719bcff485
vreverberi <viviana.reverberi@gmail.com>
2022-08-01 21:32:46 -0300

    Tarea M5U4

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect ('admin/login');
+  res.render('admin/novedades', {
+    layout:'admin/layout',
+    persona: req.session.nombre
+  });
 });
 
 module.exports = router;
