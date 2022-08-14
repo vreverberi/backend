421ab517a8f9f79cf2fea7611a656b2807ade6a2
vreverberi <viviana.reverberi@gmail.com>
2022-08-14 17:14:36 -0300

    Tarea M6U2

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,84 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('admin/novedades', {
-    layout:'admin/layout',
-    persona: req.session.nombre
-  });
-});
+    layout: 'admin/layout',
+    usuario: req.session.nombre,
+    novedades
+  })
+})
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
+})
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != ""  && req.body.subtitulo != "" && req.body.cuerpo != ""){
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout:'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log (error)
+    res.render('/admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargó la novedad'
+    })
+  }
+})
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+}) //cierra get de eliminar
+
+router.get('/editar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id)
+  // console.log(req.params.id) 
+  // console.log("Hasta acá llegó")
+  // console.log(novedad)
+  res.render('admin/editar', {
+    layout: 'admin/layout', 
+    novedad
+  })
+}) 
+
+router.post('/editar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+
+    await novedadesModel.editarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/editar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modificó la novedad'
+    })
+  }
+})
 
 module.exports = router;
